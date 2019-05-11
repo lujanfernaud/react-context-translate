9278d547599e20e49fdd75d9f8118bfae79511ff
lujanfernaud <lujanfernaud@gmail.com>
2019-05-11 08:16:01 +0100

    Add form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import UserCreate from './UserCreate'
 
 class App extends Component {
+  state = {
+    language: 'english',
+  }
+
+  handleLanguageChange = language => {
+    this.setState({ language })
+  }
+
   render() {
     return (
-      <div>
-        <div>App</div>
+      <div className="ui container">
+        <div>
+          Select a language:{' '}
+          <i className="flag us" onClick={() => this.handleLanguageChange('english')} />
+          <i className="flag nl" onClick={() => this.handleLanguageChange('dutch')} />
+        </div>
+
+        <br />
+
+        <UserCreate />
       </div>
     )
   }
